a646039380fc1be56f5ab1a9476e39d5f86a94f0
cr21 <cr.tagadiya@gmail.com>
2024-11-09 17:14:06 -0600

    aws github workflow updated with t3.xlarge-cpu v3

--- start_script.sh
+++ start_script.sh
@@ -8,7 +8,7 @@ curl https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip -o awscliv2.zip
 unzip awscliv2.zip
 ./aws/install --update
 nvidia-smi
-python3 src/train.py experiment=bird_classifier_ex  trainer=gpu +trainer.log_every_n_steps=1
+python3 src/train.py experiment=bird_classifier_ex  trainer=cpu +trainer.log_every_n_steps=1
 #python3 src/train.py experiment=bird_classifier_ex +trainer.log_every_n_steps=1
 aws s3 ls pytorch-model-emlov4-predictions/bird_classification --recursive
 python3 src/infer.py data=birddata inference=bird_infer_aws
