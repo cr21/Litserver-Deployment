dbdcd3abf3926ebea9982484714101a62d47a41e
cr21 <cr.tagadiya@gmail.com>
2024-11-09 18:57:13 -0600

    aws github workflow updated with t3.xlarge-cpu v5

--- start_script.sh
+++ start_script.sh
@@ -8,8 +8,19 @@ curl https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip -o awscliv2.zip
 unzip awscliv2.zip
 ./aws/install --update
 nvidia-smi
+echo "Start Training!!!!"
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 python3 src/train.py experiment=bird_classifier_ex  trainer=cpu +trainer.log_every_n_steps=1
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 #python3 src/train.py experiment=bird_classifier_ex +trainer.log_every_n_steps=1
 aws s3 ls pytorch-model-emlov4-predictions/bird_classification --recursive
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "RUNNING aws s3 ls pytorch-model-emlov4-predictions/bird_classification --recursive"
 python3 src/infer.py data=birddata inference=bird_infer_aws
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "START INFERNCING"
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "RUNNING aws s3 ls pytorch-model-emlov4-predictions/bird_classification --recursive"
 aws s3 ls pytorch-model-emlov4-predictions/bird_classification --recursive
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
