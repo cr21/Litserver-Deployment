1b6a98861222eed66fd24e11beeb2a4902fb2e9e
cr21 <cr.tagadiya@gmail.com>
2024-11-24 01:38:27 -0600

    clean up final before deployment servewr

--- merge_data.sh
+++ merge_data.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
 # Create the main output directory
-mkdir -p data/merged_food_101
 
-# Loop through all food categories (using test directory as reference)
-for category in data/food_101_data/test/*; do
+
+# Loop through all bird categories (using Train directory as reference)
+for category in /Users/chiragtagadiya/MyProjects/EMLO_V4_projects/deployment_projects/DVC-pytorch-lightning-MLOps/data/Bird200/Train/*; do
     # Get the base category name
     category_name=$(basename "$category")
     
     # Create category directory in merged folder
-    mkdir -p "data/merged_food_101/$category_name"
+    mkdir -p "data/merged_birds/$category_name"
     
-    # Copy images from test directory
-    cp -r "data/food_101_data/test/$category_name"/* "data/merged_food_101/$category_name/"
+    # Copy images from Train directory
+    cp -r "data/Bird200/Train/$category_name"/* "data/merged_birds/$category_name/"
     
-    # Copy images from train directory
-    cp -r "data/food_101_data/train/$category_name"/* "data/merged_food_101/$category_name/"
-    
-    # Copy images from val directory
-    cp -r "data/food_101_data/val/$category_name"/* "data/merged_food_101/$category_name/"
+    # Copy images from Test directory
+    cp -r "data/Bird200/Test/$category_name"/* "data/merged_birds/$category_name/"
 done
 
-echo "Data merging completed!"
+echo "Bird data merging completed!"
